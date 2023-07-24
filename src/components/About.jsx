ce264b8448e1cf01ec35fed2ed7ae1bec02b5e79
ahuja-shivam <ahujas1203.com>
2023-07-24 12:35:31 +0530

    news api

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,10 @@ export default class News extends Component {
             "name": "BBC News"
         },
         "author": "BBC News",
-        "title": "Just Smile",
-        "description": "Just smile for a while. We are loading news for you",
-        "url": "https://www.readersdigest.ca/culture/10-short-jokes-anyone-can-remember/",
-        "urlToImage": "https://api.memegen.link/images/pigeon/Engineer/_/Is_this_Photoshop~q.png?style=https://i.imgur.com/W0NXFpQ.png&width=800&token=cy49tv234bu3jzgw587o",
+        "title": "API key is not bought",
+        "description": "The API used here is of free version, which can't be used in the hosted website",
+        "url": "https://newsapi.org/docs",
+        "urlToImage": "https://th.bing.com/th/id/OIP.WeNbGUA1yDHnL-3nrLNzKAHaEK?pid=ImgDet&rs=1",
         "publishedAt": "2022-07-28T07:37:26.1809038Z",
         "content": "Kherson's Antonivskiy Bridge is half a mile long and is one\r\nof two key routes spanning the Dnipro River.\r\nBut, although its still standing, the crossing is now completely\r\nunusable after damage from… [+605 chars]"
     },
@@ -28,13 +28,22 @@ export default class News extends Component {
     async componentDidMount() {
         this.setState({loading:true});
         let data = await fetch(`https://newsapi.org/v2/everything?q=fashion&apiKey=9ea305f0f2be4cf086c49ca1622d29b3&page=${this.state.page}&pagesize=18`);
-        
-        let json_data = await data.json();
-        this.setState({
-            articles: json_data.articles,
-            totalResult: json_data.totalResults,
-            loading: false
-        })
+        if(data.status === 200) { 
+            let json_data = await data.json();
+            console.log(data.status)
+            this.setState({
+                articles: json_data.articles,
+                totalResult: json_data.totalResults,
+                loading: false
+            })
+        }   
+        else{
+            this.setState({
+                articles: this.article,
+                totalResult: 0,
+                loading: false
+            }) 
+        }
     }
     handleNext = async () => {
         this.setState({loading:true});
@@ -64,9 +73,8 @@ export default class News extends Component {
                 {this.state.loading}
                     {!this.state.loading && 
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 my-3">
-                        {
+                        {   
                             this.state.articles.map((element) => {
-
                                     return <NewsItem key={element.url} title={element.title} description={element.description} image={element.urlToImage} newsUrl={element.url} />
                                 }
                             )
